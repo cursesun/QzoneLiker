23947327aac3a4bf33a62dcf25d64d3b7429654f
ruochenxing <sherlock_65535@qq.com>
2015-09-12 01:11:28 +0800

    no

--- jsondemo.py
+++ jsondemo.py
@@ -1,32 +1,22 @@
 #coding=utf-8
 import MySQLdb
 global conn,cur
-def saveemotion(values):
+def getfriendsbyvisited(visited):
     try:
-        cur.executemany('insert into qz_emotion(`qq`,`tid`,`content`,`create_time`,`like_num`) values(%s,%s,%s,%s,%s)',values)
-        conn.commit()
+        aa=cur.execute('select * from qz_friend where visited=%s limit 170,180',visited)
+	info = cur.fetchmany(aa)
+	return info
     except MySQLdb.Error,e:
         print "Mysql Error %d: %s" % (e.args[0], e.args[1])
-def savecomment(values):
-    try:
-        cur.executemany('insert into em_comment(`qq`,`name`,`content`,`create_time`) values(%s,%s,%s,%s)',values)
-	conn.commit()
-    except MySQLdb.Error,e:
-        print "Mysql Error %d: %s" % (e.args[0], e.args[1])
+    return None
 try:
     conn=MySQLdb.connect(host='127.0.0.1',user='root',passwd='moshu521',port=3306,charset='utf8')
     cur=conn.cursor()
     conn.select_db('qzone')
-    values=[]
-    for i in range(0,10):
-        values.append(("2421181819","7b4d5090b19bc4552b0c0200","hehe","2012-9-23 12:23:32",i))
-    saveemotion(values)
-    values1=[]
-    for j in range(0,10):
-        values1.append(("2421181819","张三",i,"2012-9-23 12:23:32"))
-    savecomment(values1)
-    cur.close()
-    conn.close()
+    temp=getfriendsbyvisited(-1)
+    if temp!=None and len(temp)>0:
+        for t in temp:
+	    print t[0],t[1]
 except MySQLdb.Error,e:
     print "Mysql Error %d: %s" % (e.args[0], e.args[1])
 
